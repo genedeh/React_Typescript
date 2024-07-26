c21d0d0d4e73c2de97c2fd2861b05cd2c723682a
genedeh <genesis.enedeh@gmail.com>
2024-07-27 00:30:13 +0100

    Made messageCount in Greet.component.tsx optional.

--- react-typescript-demo/src/components/Greet.component.tsx
+++ react-typescript-demo/src/components/Greet.component.tsx
@@ -1,18 +1,19 @@
 type GreetProps = {
     name: string,
-    messageCount: number,
+    messageCount?: number,
     isLoggedIn: boolean
 }
 
 const Greet = (props: GreetProps) => {
+    const { messageCount = 0 } = props
     return (
         <div>
-            
+
             <h2>
                 {
                     props.isLoggedIn ? `Welcome ${props.name}! You Have ${props.messageCount} unread messages.` : "Welcome Guest"
                 }
-                </h2>
+            </h2>
         </div>
     )
 }
