46906197453e72174278441f5e15f6a390f08e3c
genedeh <genesis.enedeh@gmail.com>
2024-07-26 19:30:02 +0100

    Created a Person Component

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Greet from './components/Greet.component';
-
+import Person from './components/Person.component';
 function App() {
+  const personName = {
+    first: "Genesis",
+    last: "Enedeh"
+  }
   return (
     <div className="App">
       <Greet name="Genesis" messageCount={20} isLoggedIn={true} />
+      <Person name={personName} />
     </div>
   );
 }
---
+++ react-typescript-demo/src/components/Person.component.tsx
@@ -0,0 +1,13 @@
+type PersonProps = {
+    name: {
+        first: string
+        last: string
+    }
+}
+
+const Person = (props: PersonProps) => {
+    return (
+        <div>{props.name.first} {props.name.last}</div>
+    )
+}
+export default Person;
