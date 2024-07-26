14a89a435aa864f890f2b317d22c0adb8efe5453
genedeh <genesis.enedeh@gmail.com>
2024-07-26 19:23:35 +0100

    Configured Greet Component in using Typescript Props...

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Greet from './components/Greet.component';
 
 function App() {
   return (
     <div className="App">
-      
+      <Greet name="Genesis" messageCount={20} isLoggedIn={true} />
     </div>
   );
 }
--- react-typescript-demo/src/components/Greet.component.tsx
+++ react-typescript-demo/src/components/Greet.component.tsx
@@ -1,7 +1,18 @@
-const Greet = () => {
+type GreetProps = {
+    name: string,
+    messageCount: number,
+    isLoggedIn: boolean
+}
+
+const Greet = (props: GreetProps) => {
     return (
         <div>
-            <h2>Welcome User, You Have{Math.random()} unread messages.</h2>
+            
+            <h2>
+                {
+                    props.isLoggedIn ? `Welcome ${props.name}! You Have ${props.messageCount} unread messages.` : "Welcome Guest"
+                }
+                </h2>
         </div>
     )
 }
