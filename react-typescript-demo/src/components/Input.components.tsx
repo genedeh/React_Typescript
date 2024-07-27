6afec1242712f06e6eb17384d0db750be2e12833
genedeh <genesis.enedeh@gmail.com>
2024-07-27 23:08:14 +0100

    Destructuring Of InputProps

--- react-typescript-demo/src/components/Input.components.tsx
+++ react-typescript-demo/src/components/Input.components.tsx
@@ -3,12 +3,12 @@ type InputProps = {
     handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Input = (props: InputProps) => {
+const Input = ({ value, handleChange }: InputProps) => {
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value)
     }
     return (
-        <input type="text" value={props.value} onChange={handleInputChange} />
+        <input type="text" value={value} onChange={handleInputChange} />
     )
 }
 
